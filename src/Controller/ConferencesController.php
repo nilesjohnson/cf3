0712510dfa2040ff2515bc7e3d7c6e9880282f34
Seth Johnson <sethj@centerofthewest.org>
2018-07-25 05:37:04 +0200

    old stuff

--- src/Controller/ConferencesController.php
+++ src/Controller/ConferencesController.php
@@ -3,106 +3,31 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-/**
- * Conferences Controller
- *
- * @property \App\Model\Table\ConferencesTable $Conferences
- *
- * @method \App\Model\Entity\Conference[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
+
 class ConferencesController extends AppController
 {
+public function beforeFilter(Event $event) {
+		parent::beforeFilter($event);
+		//we will use admin prefix for editing stuff
+		$this->Auth->allow();
+}
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
     public function index()
     {
-        $conferences = $this->paginate($this->Conferences);
-        $this->set(compact('conferences'));
+		$this->set('view_title','Upcoming Meetings');
+		$query=$this->Conferences->find('all')->where(['end_date > '=>date('Y-m-d', strtotime("-1 week"))]);
+		$conferences = $this->paginate($query);
+		$this->set(compact('conferences'));
+		//
+	/*	$this->render_list(['tagstring' => $tagstring,
+			     'conditions' => array (
+						    "Conference.end_date >" => date('Y-m-d', strtotime("-1 week"))),
+			     ));*/
+				 
+	   // BAKED
+	   // $conferences = $this->paginate($this->Conferences);
+       // $this->set(compact('conferences'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Conference id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $conference = $this->Conferences->get($id, [
-            'contain' => ['Tags']
-        ]);
-
-        $this->set('conference', $conference);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $conference = $this->Conferences->newEntity();
-        if ($this->request->is('post')) {
-            $conference = $this->Conferences->patchEntity($conference, $this->request->getData());
-            if ($this->Conferences->save($conference)) {
-                $this->Flash->success(__('The conference has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The conference could not be saved. Please, try again.'));
-        }
-        $tags = $this->Conferences->Tags->find('list', ['limit' => 200]);
-        $this->set(compact('conference', 'tags'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Conference id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $conference = $this->Conferences->get($id, [
-            'contain' => ['Tags']
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $conference = $this->Conferences->patchEntity($conference, $this->request->getData());
-            if ($this->Conferences->save($conference)) {
-                $this->Flash->success(__('The conference has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The conference could not be saved. Please, try again.'));
-        }
-        $tags = $this->Conferences->Tags->find('list', ['limit' => 200]);
-        $this->set(compact('conference', 'tags'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Conference id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $conference = $this->Conferences->get($id);
-        if ($this->Conferences->delete($conference)) {
-            $this->Flash->success(__('The conference has been deleted.'));
-        } else {
-            $this->Flash->error(__('The conference could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
+    
 }
